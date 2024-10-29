4634c03d9a5d3ea067b2c5c2e4db3441c84a5ab4
Artur Gubaidullin <lapteuh@gmail.com>
2024-10-29 17:52:02 +0000

    Add preact to web-app

--- web-app/astro.config.mjs
+++ web-app/astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: true,
   },
 
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
 });
